d3e603f848827f08c5f8220dd46a605a6b91ce9a
XalilCode <162160441+XalilCode@users.noreply.github.com>
2024-03-05 00:38:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import panda as pd
+import panda pd
 
 cnx = st.connection("snowflake")
 session = cnx.session()
